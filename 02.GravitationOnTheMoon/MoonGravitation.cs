387b2f9fc3d90a0faaf787f38c82d5c80d01c260
GogoK <georgiakanchev@gmail.com>
2015-09-22 02:28:03 +0300

    2-moon gravitation; 3-value diviable by seven and five without remainder

---
+++ 02.GravitationOnTheMoon/MoonGravitation.cs
@@ -0,0 +1,21 @@
+﻿/*  The gravitational field of the Moon is approximately 17% of that on the Earth. 
+    Write a program that calculates the weight of a man on the moon by a given weight on the Earth.
+ */
+
+using System;
+
+class MoonGravitation
+{
+    static void Main()
+    {
+        string input;
+        double value;
+        do
+        {
+            Console.Write("Enter value: ");
+            input = Console.ReadLine();
+        } while (!double.TryParse(input, out value));
+        double result = value * 0.17;
+        Console.WriteLine(result);
+    }
+}
---
+++ 03.DivideBy7And5/DivideBy7And5.cs
@@ -0,0 +1,21 @@
+﻿/*  Write a Boolean expression that checks for given integer if it can be divided (without remainder) by 7 and 5 in the same time.
+ */
+
+using System;
+
+class DivideBy7And5
+{
+    static void Main()
+    {
+        string input;
+        int value;
+        do
+        {
+            Console.Write("Enter value: ");
+            input = Console.ReadLine();
+        } while (!int.TryParse(input, out value));
+
+        bool checker = (value % 7 == 0) && (value % 5 == 0);
+        Console.WriteLine(checker);
+    }
+}
