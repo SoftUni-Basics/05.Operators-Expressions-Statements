2561f93753055bc317c64e7dacd628ee236f900f
GogoK <georgiakanchev@gmail.com>
2015-09-22 02:54:15 +0300

    4-rectangle perimeter and area; 5-check third digit

---
+++ 04.Rectangles/Rectangles.cs
@@ -0,0 +1,21 @@
+﻿/*  Write an expression that calculates rectangle’s perimeter and area by given width and height.
+ */
+
+using System;
+
+class Rectangles
+{
+    static void Main()
+    {
+        Console.Write("Enter rectangle width: ");
+        double width = double.Parse(Console.ReadLine());
+        Console.Write("Enter rectangle height: ");
+        double height = double.Parse(Console.ReadLine());
+
+        double perimeter = 2 * (width + height);
+        double area = width * height;
+
+        Console.WriteLine("Perimeter = {0}", perimeter);
+        Console.WriteLine("Area = {0}", area);
+    }
+}
---
+++ 05.ThirdDigitIs7/CheckThirdDigit.cs
@@ -0,0 +1,15 @@
+﻿/*  Write an expression that checks for given integer if its third digit from right-to-left is 7.
+ */
+
+using System;
+
+class CheckThirdDigit
+{
+    static void Main()
+    {
+        Console.Write("Enter value: ");
+        int input = int.Parse(Console.ReadLine());
+        input /= 100;
+        Console.WriteLine(input % 10 == 7);
+    }
+}
