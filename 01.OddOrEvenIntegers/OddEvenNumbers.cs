0d9fc5c61891add0dda3be2356c5e4cf038ab395
GogoK <georgiakanchev@gmail.com>
2015-09-22 01:59:13 +0300

    odd or even number

---
+++ 01.OddOrEvenIntegers/OddEvenNumbers.cs
@@ -0,0 +1,21 @@
+﻿/*  Write an expression that checks if given integer is odd or even.
+ */
+
+using System;
+
+class OddEvenNumbers
+{
+    static void Main()
+    {
+        string input;
+        int number;
+        do
+        {
+            Console.Write("Enter integet value: ");
+            input = Console.ReadLine();
+        } while (!int.TryParse(input, out number));
+        bool checker = number % 2 == 1;
+        Console.WriteLine("Number({0}) is Odd?", number);
+        Console.WriteLine(checker);
+    }
+}
